39cfc6804b6d7474b1a624466daf90c6d079ade1
kj22k0m <andrew345345@yandex.ru>
2023-09-07 22:40:05 +0300

    chore: add reverseBetween func

--- list/tools.go
+++ list/tools.go
@@ -31,3 +31,28 @@ func PrintLinkedList(list *Node) {
 		list = list.Next
 	}
 }
+
+// ReverseNodesValuesBetween helps to swap near right and left nodes values between their node.
+func ReverseNodesValuesBetween(head *Node, left int, right int) *Node {
+	if head == nil || left == right {
+		return head
+	}
+
+	dummy := &Node{0, head}
+	prev := dummy
+
+	for i := 0; i < left-1; i++ {
+		prev = prev.Next
+	}
+
+	current := prev.Next
+
+	for i := 0; i < right-left; i++ {
+		nextNode := current.Next
+		current.Next = nextNode.Next
+		nextNode.Next = prev.Next
+		prev.Next = nextNode
+	}
+
+	return dummy.Next
+}
